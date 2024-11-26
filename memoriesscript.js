c1849b5326f86e1dbe34d52326b41bec6bf5c8a1
Abhishek Wadile <130994831+Abhiwadile@users.noreply.github.com>
2024-11-26 18:21:36 +0530

    Add files via upload

---
+++ memoriesscript.js
@@ -0,0 +1,6 @@
+function showSurprise() {
+    const surpriseMessage = document.getElementById('surprise-message');
+    surpriseMessage.classList.add('visible');
+    surpriseMessage.classList.remove('hidden');
+  }
+  
